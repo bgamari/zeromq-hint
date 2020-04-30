ab3ee060f8557c103763637e5fa8453c191c3d3c
Ben Gamari <ben@smart-cactus.org>
2020-04-30 16:11:12 -0400

    It builds

--- Main.hs
+++ Main.hs
@@ -2,12 +2,17 @@
 
 
 module Main where
+
+import Control.Monad
+import Control.Monad.Catch
 import System.IO
-import Control.Monad --do
-import Data.Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson ((.=), (.:))
 -- import Data.Typeable
-import System.ZMQ4.Monadic
-import Language.Haskell.Interpreter --hint for evaluation
+import System.ZMQ4.Monadic as ZMQ
+ 
+ -- hint for evaluation
+import Language.Haskell.Interpreter as Hint
 
 -- data type import
 import Data.Either --fromRight
@@ -15,42 +20,58 @@ import Prelude hiding (lookup)
 import Data.Maybe(fromMaybe)
 import qualified Data.Text as T
 import Data.Text (Text, pack)
-import Data.ByteString.Char8 (pack, unpack)
-import Data.Text.Encoding (encodeUtf8)
-import Data.Map --decode
--- Aeson's "encode" to json generates lazy bytestrings
-import qualified Data.ByteString as B
+import qualified Data.Map as Map
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BSL
-
+import Control.Monad.Trans.Class
 
 -- JSON encoding section
-data Outdata = Outdata { x1 :: String }
-             deriving (Show)
+data Request
+  = Evaluate { reqInput :: Text }
 
-instance ToJSON Outdata where
-  toJSON (Outdata xV) = object [ "output" .= xV]
+instance Aeson.FromJSON Request where
+    parseJSON = Aeson.withObject "request" $ \o ->
+        Evaluate <$> o .: "input"
 
-packStr :: String -> B.ByteString
-packStr = encodeUtf8 . T.pack
+data EvalResult
+  = Success { x1 :: String }
+  | Error { errmsg :: String }
+  deriving (Show)
 
-main ::IO ()
+instance Aeson.ToJSON EvalResult where
+    toJSON (Success xV) = Aeson.object [ "output" Aeson..= xV]
+    toJSON (Error msg)  = Aeson.object [ "error" Aeson..= msg]
 
+main :: IO ()
 main = runZMQ $ do
-    responder <- socket Pair
-    bind responder "tcp://*:5558"
+    responder <- ZMQ.socket Pair
+    ZMQ.bind responder "tcp://*:5558"
     liftIO $ do
         putStrLn "tcp://127.0.0.1:5558"
         hFlush stdout
     
-    
-    forever $ do
-        message  <- receive responder
-        let msg = BSL.pack $ unpack message
-        let decodedMsg = decode msg :: Maybe (Map Text Text)
-        let inputMsg = fromMaybe "" $ lookup "input" $ fromMaybe (fromList []) decodedMsg
-        let toEvalStr = T.unpack inputMsg
+    res <- runInterpreter $ do
+        Hint.setImports ["Prelude"]
+        forever $ do
+            req <- lift $ readRequest responder
+            result <- evaluateRequest req
+            lift $ ZMQ.send responder [] $ BSL.toStrict $ Aeson.encode result
         
-        r <- liftIO $ runInterpreter $ do { setImports ["Prelude"]; eval toEvalStr}
-        let res  = fromRight "" (r)
-        let reply = Outdata res
-        send responder [] $ packStr $ BSL.unpack (encode reply)
+        return ()
+            
+    -- TODO: Handle failure
+    liftIO $ print res
+
+readRequest :: ZMQ.Socket z ZMQ.Pair -> ZMQ z Request
+readRequest sock = do
+    message <- ZMQ.receive sock
+    case Aeson.eitherDecode $ BSL.fromStrict message of
+        Left err  -> fail "invalid request" -- TODO: Handle failure
+        Right req -> return req
+
+evaluateRequest :: MonadInterpreter m => Request -> m EvalResult
+evaluateRequest (Evaluate input) =
+    handle onError $ fmap Success $ Hint.eval $ T.unpack input
+  where
+    onError :: Monad m => InterpreterError -> m EvalResult
+    onError = return . Error . show
